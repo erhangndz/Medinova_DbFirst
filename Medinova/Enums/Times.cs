701f201f2110bcee2f6dfd005c85fe0be595f821
erhangndz <erhangndz@live.com>
2025-12-18 22:14:03 +0300

    updated

---
+++ Medinova/Enums/Times.cs
@@ -0,0 +1,12 @@
+﻿namespace Medinova.Enums
+{
+    public static class Times
+    {
+        public const string Time9 = "9:00";
+        public const string Time930 = "9:30";
+        public const string Time10 = "10:00";
+        public const string Time1030 = "10:30";
+        public const string Time11 = "11:00";
+        public const string Time1130 = "11:30";
+    }
+}
